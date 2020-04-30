7aead4e1a5e3dde2c5e4ae765f103ed691923bac
RoryRed <60267332+RoryRed@users.noreply.github.com>
2020-04-30 03:31:31 +0100

    Add files via upload

--- Project/analysis.py
+++ Project/analysis.py
@@ -22,10 +22,10 @@ def SummarySepalWidthVeriscolor():
     return data.versicolorsw.describe()
 
 def SummaryPetalWidthVeriscolor():
-    return data.veriscolorpw.describe()
+    return data.versicolorpw.describe()
 
 def SummaryPetallengthVeriscolor():
-    return data.veriscolorpl.describe()
+    return data.versicolorpl.describe()
 
 def SummarySepalLengthVirginica():
     return data.virginicasl.describe()
@@ -34,10 +34,10 @@ def SumarryViriginicaSepalWidth():
     return data.virginicasw.describe()
 
 def SumamrryVirgincaPetalLength():
-    return data.virigincapl.describe()
+    return data.virginicapl.describe()
 
 def SummarryVirgincaPetalwidth ():
-    return data.virigincapw.describe()
+    return data.virginicapw.describe()
 
 Iris_VersicolorsSepalLength =str(SummarySepalLengthVeriscolor())
 Iris_setosaPetalLength = str(SumarryPetalLength())
@@ -48,9 +48,9 @@ Iris_VersicolorsSepalWidth = str(SummarySepalWidthVeriscolor())
 Iris_VersicolorsPetalWidth = str(SummaryPetalWidthVeriscolor())
 Iris_VersicolorsPetalLength = str(SummaryPetallengthVeriscolor())
 Iris_ViginicaSepalLength = str(SummarySepalLengthVirginica())
-Iris_ViginicaSepalWidth = str(SumarryViriginicaPetalWidth())
+Iris_ViginicaSepalWidth = str(SumarryViriginicaSepalWidth())
 Iris_ViginicaPetalLength = str(SumamrryVirgincaPetalLength())
-Iris_ViginicaPetalwidth = str(SumamrryVirgincaPetalwidth ())
+Iris_ViginicaPetalwidth = str(SummarryVirgincaPetalwidth())
 Iris_VeriscolorSepalLength = str(SummarySepalLengthVeriscolor())
  #converts series into string
 file = open("analysis.txt", "w") #opens text file and makes it writable 
@@ -69,3 +69,55 @@ file.write(Iris_ViginicaPetalwidth)
 file.close()
 
 
+#histogram
+
+data = pd.read_csv("Data.csv")
+plt.title("Iris-setosa Sepal Length")
+plt.hist(data.setosasl,  rwidth = 0.5)
+plt.savefig("Iris-setosa Sepal Length.png")
+
+plt.hist(data.setosasw, rwidth = 0.5)
+plt.title("Iris-setosa Sepal Width")
+plt.savefig("Iris-setosa Sepal Width.png")
+
+
+plt.hist(data.setosapl, rwidth = 0.5)
+plt.title("Iris-setosa Petal Length")
+plt.savefig("Iris-setosa Petal Length")
+
+
+plt.hist(data.setosapw, rwidth = 0.5 )
+plt.title("Iris-setosa Petal Width")
+plt.savefig("Iris-setosa Petal Width.png")
+
+plt.hist(data.versicolorsl, rwidth = 0.5)
+plt.title("Iris-versicolor Sepal Length")
+plt.savefig("Iris-versicolor Sepal Length.png")
+
+plt.hist(data.versicolorsw , rwidth = 0.5 )
+plt.title("Iris-versicolor Sepal Width ")
+plt.savefig("Iris-versicolor Sepal Width.png")
+
+plt.hist(data.versicolorpl, rwidth = 0.5)
+plt.title("Iris-versicolor Sepal Length")
+plt.savefig("Iris-versicolor Sepal Length.png")
+
+plt.hist(data.versicolorpw, rwidth = 0.5)
+plt.title("Iris-virginica Petal Width")
+plt.savefig("Iris-virginica Petal Width.png")
+
+plt.hist(data.virginicasl, rwidth = 0.5 )
+plt.title("Iris-virginica Sepal Length")
+plt.savefig("Iris-virginica Sepal Length.png")
+
+plt.hist(data.virginicasw, rwidth = 0.5)
+plt.title("Iris-virginica Sepal Width")
+plt.savefig("Iris-virginica Sepal Width.png")
+
+plt.hist(data.virginicapl, rwidth = 0.5)
+plt.title("Iris-virginica Petal Length")
+plt.savefig("Iris-virginica Petal Length.png")
+
+plt.hist(data.virginicapw, rwidth = 0.5)
+plt.title("Iris-virginica Petal Width")
+plt.savefig("Iris-virginica Petal Width.png")
